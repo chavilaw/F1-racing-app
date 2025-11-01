ed7d473dfa5aed6afc0a6b3186fc599159f8b775
chavilahwong <chavilahchanaw@gmail.com>
2025-11-02 00:37:44 +0200

    changes in server.js

--- server.js
+++ server.js
@@ -23,8 +23,9 @@ app.get('/', (req, res) => {
   res.sendFile('index.html');
 });
 
-server.listen(3000, () => {
+server.listen(3000, '0.0.0.0', () => {
   console.log('Server listening on http://localhost:3000');
+  console.log('Server accessible from network on port 3000');
   console.log(`Persistence: ${PERSIST ? 'ENABLED' : 'DISABLED'} (start with --persist to enable)`);
 });
 
